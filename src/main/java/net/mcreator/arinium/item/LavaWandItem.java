45038efc34383dc76d98c81144c3a0ef3e244aa9
AriniumTeam <ismab@192.168.1.28>
2021-07-22 00:39:20 +0200

    Nerf Des Dyna Création De La Lava Wand et je sais plus

---
+++ src/main/java/net/mcreator/arinium/item/LavaWandItem.java
@@ -0,0 +1,81 @@
+
+package net.mcreator.arinium.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.World;
+import net.minecraft.util.Hand;
+import net.minecraft.util.ActionResult;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.arinium.procedures.LavaWandProcedureProcedure;
+import net.mcreator.arinium.itemgroup.UtilitiesItemGroup;
+import net.mcreator.arinium.AriniumModElements;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@AriniumModElements.ModElement.Tag
+public class LavaWandItem extends AriniumModElements.ModElement {
+	@ObjectHolder("arinium:lava_wand")
+	public static final Item block = null;
+	public LavaWandItem(AriniumModElements instance) {
+		super(instance, 353);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(UtilitiesItemGroup.tab).maxDamage(5).rarity(Rarity.COMMON));
+			setRegistryName("lava_wand");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public boolean hasEffect(ItemStack itemstack) {
+			return true;
+		}
+
+		@Override
+		public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity entity, Hand hand) {
+			ActionResult<ItemStack> ar = super.onItemRightClick(world, entity, hand);
+			ItemStack itemstack = ar.getResult();
+			double x = entity.getPosX();
+			double y = entity.getPosY();
+			double z = entity.getPosZ();
+			{
+				Map<String, Object> $_dependencies = new HashMap<>();
+				$_dependencies.put("x", x);
+				$_dependencies.put("y", y);
+				$_dependencies.put("z", z);
+				$_dependencies.put("world", world);
+				LavaWandProcedureProcedure.executeProcedure($_dependencies);
+			}
+			return ar;
+		}
+	}
+}
--- src/main/java/net/mcreator/arinium/procedures/LavaWandProcedureProcedure.java
+++ src/main/java/net/mcreator/arinium/procedures/LavaWandProcedureProcedure.java
@@ -1,8 +1,8 @@
 package net.mcreator.arinium.procedures;
 
-import net.minecraft.world.World;
 import net.minecraft.world.IWorld;
-import net.minecraft.world.Explosion;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.block.Blocks;
 
 import net.mcreator.arinium.AriniumModElements;
 import net.mcreator.arinium.AriniumMod;
@@ -10,38 +10,36 @@ import net.mcreator.arinium.AriniumMod;
 import java.util.Map;
 
 @AriniumModElements.ModElement.Tag
-public class AriniumDynaRangedItemUsedProcedure extends AriniumModElements.ModElement {
-	public AriniumDynaRangedItemUsedProcedure(AriniumModElements instance) {
-		super(instance, 269);
+public class LavaWandProcedureProcedure extends AriniumModElements.ModElement {
+	public LavaWandProcedureProcedure(AriniumModElements instance) {
+		super(instance, 354);
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
 		if (dependencies.get("x") == null) {
 			if (!dependencies.containsKey("x"))
-				AriniumMod.LOGGER.warn("Failed to load dependency x for procedure AriniumDynaRangedItemUsed!");
+				AriniumMod.LOGGER.warn("Failed to load dependency x for procedure LavaWandProcedure!");
 			return;
 		}
 		if (dependencies.get("y") == null) {
 			if (!dependencies.containsKey("y"))
-				AriniumMod.LOGGER.warn("Failed to load dependency y for procedure AriniumDynaRangedItemUsed!");
+				AriniumMod.LOGGER.warn("Failed to load dependency y for procedure LavaWandProcedure!");
 			return;
 		}
 		if (dependencies.get("z") == null) {
 			if (!dependencies.containsKey("z"))
-				AriniumMod.LOGGER.warn("Failed to load dependency z for procedure AriniumDynaRangedItemUsed!");
+				AriniumMod.LOGGER.warn("Failed to load dependency z for procedure LavaWandProcedure!");
 			return;
 		}
 		if (dependencies.get("world") == null) {
 			if (!dependencies.containsKey("world"))
-				AriniumMod.LOGGER.warn("Failed to load dependency world for procedure AriniumDynaRangedItemUsed!");
+				AriniumMod.LOGGER.warn("Failed to load dependency world for procedure LavaWandProcedure!");
 			return;
 		}
 		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
-		if (world instanceof World && !((World) world).isRemote) {
-			((World) world).createExplosion(null, (int) x, (int) y, (int) z, (float) 5, Explosion.Mode.BREAK);
-		}
+		world.setBlockState(new BlockPos((int) x, (int) y, (int) z), Blocks.LAVA.getDefaultState(), 3);
 	}
 }
